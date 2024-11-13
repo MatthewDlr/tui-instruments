0c1697765086b1babe2750ce19079e1a4590a44e
Matthieu <matthieu.delarue5@gmail.com>
2024-11-12 17:52:07 -0800

    🐛✨ Fix bugs & add support for choosing inputs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,30 @@
 import { Component, effect, inject } from "@angular/core";
 import { ArduinoService } from "./services/arduino.service";
 import { YogaMatComponent } from "./components/yoga-mat/yoga-mat.component";
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [YogaMatComponent],
+  imports: [YogaMatComponent, CommonModule],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.scss",
 })
 export class AppComponent {
   title = "tui-instruments";
-  
+
   private readonly arduinoService = inject(ArduinoService);
   public isArduinoConnected = this.arduinoService.isArduinoConnected();
   public isDemoMode = this.arduinoService.isDemoMode();
   public sensorMatrix = this.arduinoService.sensorMatrix();
+  public inputMode = this.arduinoService.inputMode();
 
   constructor() {
     effect(() => {
       this.isArduinoConnected = this.arduinoService.isArduinoConnected();
       this.isDemoMode = this.arduinoService.isDemoMode();
       this.sensorMatrix = this.arduinoService.sensorMatrix();
+      this.inputMode = this.arduinoService.inputMode();
     });
   }
 
@@ -31,5 +34,9 @@ export class AppComponent {
 
   public toggleDemoMode() {
     this.arduinoService.toggleDemoMode();
+  }
+
+  public toggleInputMode(newMode: "capacitive" | "force" | "both") {
+    this.arduinoService.inputMode.set(newMode);
   }
 }
--- src/app/services/arduino.service.ts
+++ src/app/services/arduino.service.ts
@@ -11,12 +11,12 @@ export class ArduinoService {
   private port: WritableSignal<unknown> = signal(undefined);
   public isArduinoConnected: Signal<boolean> = computed(() => this.port() !== undefined);
   public isDemoMode: WritableSignal<boolean> = signal(false);
+  public inputMode: WritableSignal<"capacitive" | "force" | "both"> = signal("both");
 
   public sensorMatrix: WritableSignal<boolean[][]> = signal([
     [false, false, false, false],
     [false, false, false, false],
   ]);
-  public forceSensorMap = new Map<string, number>();
 
   constructor() {
     this.serial = new NgxSerial(this.dataHandler.bind(this));
@@ -30,25 +30,42 @@ export class ArduinoService {
   }
 
   private dataHandler(matrixString: string) {
-    const matrix: boolean[][] = [];
+    console.log(matrixString);
+    const matrix: boolean[][] = [[]];
 
-    if (matrixString.includes("A")) {
-      // Handle the case where we receive the force sensor values
+    if ((this.inputMode() === "force" || this.inputMode() === "both") && matrixString.includes("A")) {
+      if (matrixString.endsWith(",")) matrixString = matrixString.slice(0, -1);
+      const forceSensorMap = new Map<string, number>();
       const pairs = matrixString.split(",");
-      pairs.forEach(pair => {
-        const [key, value] = pair.split(":").map(item => item.trim());
-        const index = parseInt(key.substring(1), 10);
-        this.forceSensorMap.set(key, parseInt(value, 10));
 
-        if (index % 2 === 0) {
-          matrix[0][index / 2] = parseInt(value, 10) > 0;
-        } else {
-          matrix[1][Math.floor(index / 2)] = parseInt(value, 10) > 0;
+      pairs.forEach(pair => {
+        if (pair) {
+          const [key, value] = pair.split(":");
+          const numericValue = Number(value);
+          const portNumber = Number(key[1]);
+          if (!isNaN(numericValue) && !isNaN(portNumber)) {
+            forceSensorMap.set(key, numericValue);
+          }
         }
       });
+      const forceMatrix: boolean[][] = [[], []];
+      forceSensorMap.forEach((value, key) => {
+        const keyNumber = Number(key[1]);
+        if (keyNumber === 0 || keyNumber % 2 === 0) {
+          forceMatrix[0].push(value > 0);
+        } else {
+          forceMatrix[1].push(value > 0);
+        }
+      });
+      console.log(forceMatrix);
+      this.sensorMatrix.set(forceMatrix);
 
-      console.log(this.forceSensorMap);
-    } else {
+      if (this.inputMode() === "both") {
+        this.sensorMatrix.set([...this.sensorMatrix(), ...forceMatrix]);
+      } else {
+        this.sensorMatrix.set(forceMatrix);
+      }
+    } else if (this.inputMode() === "capacitive" || this.inputMode() === "both") {
       // Handle the case where we receive the capacitive sensor matrix
       const rows = matrixString.split(";");
       rows.forEach(row => {
@@ -58,10 +75,10 @@ export class ArduinoService {
 
       console.log(matrix);
       this.sensorMatrix.set(matrix);
+    }
 
-      if (matrix.length > 0) {
-        this.soundEngine.generateSoundFromMatrix(matrix);
-      }
+    if (matrix.length > 0) {
+      this.soundEngine.generateSoundFromMatrix(matrix);
     }
   }
 
